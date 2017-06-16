b5d51575036f61c3047cab6f440e7979cddebf8f
林　翼 <tsubasa_hayashi@r.recruit.co.jp>
2017-06-16 09:51:39 +0900

    Initial Commit

---
+++ WKScriptMessageHandlerSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WKScriptMessageHandlerSample
+//
+//  Created by 林　翼 on 2017/06/16.
+//  Copyright © 2017年 Tsubasa Hayashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
